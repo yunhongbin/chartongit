15e919fd7414912983e5f5c3def26d0642b3f1ac
voles2910 <776565384@qq.com>
2016-08-10 16:26:49 +0800

    ss

---
+++ src/com/yt/chart/pojo/Menu.java
@@ -0,0 +1,33 @@
+package com.yt.chart.pojo;
+
+public class Menu {
+	private String mid;
+	private String parentmid;
+	private String text;
+	private String href;
+	public String getMid() {
+		return mid;
+	}
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+	public String getParentmid() {
+		return parentmid;
+	}
+	public void setParentmid(String parentmid) {
+		this.parentmid = parentmid;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getHref() {
+		return href;
+	}
+	public void setHref(String href) {
+		this.href = href;
+	}
+	
+}
